05a3ebc24682b8fb8630e3883e8bc2151c458021
Ye <yejin@harmonycloud.com.cn>
2020-06-08 21:52:37 +0800

    test-class-JclassLib-字节码

---
+++ src/Test/TestClass.java
@@ -0,0 +1,23 @@
+package Test;
+
+
+import java.util.Objects;
+
+public class TestClass {
+
+//    static class  TreeNode{
+//        TreeNode left;
+//        TreeNode right;
+//        int value;
+//        TreeNode(){
+//        }
+//        TreeNode(int value){
+//            this.value = value;
+//        }
+//    }
+
+    public static void main(String[] args) {
+        //TreeNode objects = new TreeNode(8);
+        Object o = new Object();
+    }
+}
